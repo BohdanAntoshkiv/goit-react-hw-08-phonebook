7edaaea9a417f8f168ba6955bc23c0b22989ed62
Bohdan <Antoshkivbl@gmail.com>
2022-11-01 19:46:22 +0200

    refactoring Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,60 +1,65 @@
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState } from 'react';
 import { FormStyled, Input, Label } from './Form.styled';
 import { Button } from 'components/ContactList/ContactList.styled';
 
-export default class Form extends Component {
-    nameId = nanoid();
-    numberId = nanoid();
+export default function Form({ onSubmit }) {
+    const nameId = nanoid();
+    const numberId = nanoid();
 
-    state = {
-        name: '',
-        number: '',
-    };
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
 
-    handleChange = e => {
+    const handleChange = e => {
         const { name, value } = e.target;
-        this.setState({ [name]: value });
+        switch (name) {
+            case 'name':
+                setName(value);
+                break;
+            case 'number':
+                setNumber(value);
+                break;
+            default:
+                return;
+        }
     };
 
-    handleSubmit = e => {
+    const handleSubmit = e => {
         e.preventDefault();
-        this.props.onSubmit(this.state);
-        this.reset();
+        onSubmit({ name, number });
+        reset();
     };
 
-    reset = () => {
-        this.setState({ name: '', number: '' });
+    const reset = () => {
+        setName('');
+        setNumber('');
     };
 
-    render() {
-        const { name, number } = this.state;
-        return (
-            <FormStyled onSubmit={this.handleSubmit}>
-                <Label htmlFor={this.nameId}>Name:</Label>
-                <Input
-                    type="text"
-                    name="name"
-                    id={this.nameId}
-                    pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                    title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                    required
-                    value={name}
-                    onChange={this.handleChange}
-                />
-                <Label htmlFor={this.numberId}>Number:</Label>
-                <Input
-                    type="tel"
-                    name="number"
-                    id={this.numberId}
-                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-                    required
-                    value={number}
-                    onChange={this.handleChange}
-                />
-                <Button type="submit">Add contact</Button>
-            </FormStyled>
-        );
-    }
+    return (
+        <FormStyled onSubmit={handleSubmit}>
+            <Label htmlFor={nameId}>Name:</Label>
+            <Input
+                type="text"
+                name="name"
+                id={nameId}
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value={name}
+                onChange={handleChange}
+            />
+            <Label htmlFor={numberId}>Number:</Label>
+            <Input
+                type="tel"
+                name="number"
+                id={numberId}
+                pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                required
+                value={number}
+                onChange={handleChange}
+            />
+            <Button type="submit">Add contact</Button>
+        </FormStyled>
+    );
 }

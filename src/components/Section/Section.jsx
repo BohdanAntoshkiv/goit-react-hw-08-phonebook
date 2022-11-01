4c548ea9473482cdc0a98e1c20d9387bdcf82e6e
Bohdan <Antoshkivbl@gmail.com>
2022-11-01 19:50:04 +0200

    refactoring Section

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,17 +1,14 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import PropTypes from 'prop-types';
 import { SectionWrapp, Title } from './Section.styled';
 
-export default class Section extends Component {
-    render() {
-        const { title, children } = this.props;
-        return (
-            <SectionWrapp>
-                <Title>{title}</Title>
-                {children}
-            </SectionWrapp>
-        );
-    }
+export default function Section({ title, children }) {
+    return (
+        <SectionWrapp>
+            <Title>{title}</Title>
+            {children}
+        </SectionWrapp>
+    );
 }
 
 Section.propTypes = {

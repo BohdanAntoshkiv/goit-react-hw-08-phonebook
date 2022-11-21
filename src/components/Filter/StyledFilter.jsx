a47ce825ef1d8133148eefbc148ee7de8df254b8
Bohdan <Antoshkivbl@gmail.com>
2022-11-21 18:48:53 +0200

    add styles

--- src/components/Filter/StyledFilter.jsx
+++ src/components/Filter/StyledFilter.jsx
@@ -1,22 +1,22 @@
 import styled from 'styled-components';
 
 export const StyledFilterDiv = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 15px;
-  label {
-    /* margin-bottom: 15px; */
-  }
-  input {
-    display: block;
-    height: 25px;
-    padding: 5px;
-    border-radius: 5px;
-    border: 1px solid #c7bea2;
-    outline: none;
-    &:focus {
-      border: 1px solid #9a9483;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 15px;
+    label {
+        /* margin-bottom: 15px; */
     }
-  }
+    input {
+        display: block;
+        height: 25px;
+        padding: 5px;
+        border-radius: 5px;
+        border: 1px solid #4eafd0;
+        outline: none;
+        &:focus {
+            border: 1px solid #9a9483;
+        }
+    }
 `;

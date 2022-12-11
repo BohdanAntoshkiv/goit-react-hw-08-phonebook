51a36a6df4c6b4e88de6cf6bd84903aff6df5ac7
Bohdan <Antoshkivbl@gmail.com>
2022-12-11 21:15:57 +0200

    add styles

--- src/components/UserMenu/StyledUserMenu.js
+++ src/components/UserMenu/StyledUserMenu.js
@@ -5,14 +5,14 @@ export const StyledDiv = styled.div`
   align-items: center;
   gap: 15px;
   button {
-    border: 1px solid #db4bff;
-    background-color: #db4bff;
+    border: 1px solid rgb(78, 175, 208);
+    background-color: rgb(129, 249, 175);
     color: #ffffff;
     cursor: pointer;
     transition: ease-in 200ms;
     :hover {
-      border: 1px solid #db4bff;
-      background-color: #db4bff;
+      border: 2px solid rgb(78, 175, 208);
+      background-color: #4ca3e0;
     }
   }
 `;
